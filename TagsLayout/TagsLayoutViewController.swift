f0c7d0026dbed9220e3d8200b145a1f4e2ff069c
varunbhalla19 <varunbhalla19@gmail.com>
2022-10-09 13:16:41 +0530

    initial commit

---
+++ TagsLayout/TagsLayoutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TagsLayout
+//
+//  Created by varunbhalla19 on 09/10/22.
+//
+
+import UIKit
+
+class TagsLayoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
